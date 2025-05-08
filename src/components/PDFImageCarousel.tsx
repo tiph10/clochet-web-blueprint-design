385b7af1a916128cfcc076028ad73d36ea69587c
tiph10 <Tiphanie.moray@gmail.com>
2025-05-08 21:06:25 +0200

    Update PDFImageCarousel.tsx

--- src/components/PDFImageCarousel.tsx
+++ src/components/PDFImageCarousel.tsx
@@ -8,7 +8,8 @@ interface PDFImageCarouselProps {
 
 const PDFImageCarousel: React.FC<PDFImageCarouselProps> = ({ title }) => {
   // Génération des chemins des images ESTHETIQUE de 1 à 16
-  const images = Array.from({ length: 16 }, (_, i) => `/photos/ESTHETIQUE (${i + 1}).png`);
+  // Dans PDFImageCarousel.tsx
+const images = Array.from({ length: 16 }, (_, i) => `/photos/ESTHETIQUE-${i + 1}.png`);
   
   return <ImageCarousel images={images} title={title} />;
 };
