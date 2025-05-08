d1c83ff8cf433cbe45f876d25e7a6a7718330db6
tiph10 <Tiphanie.moray@gmail.com>
2025-05-08 20:25:09 +0200

    Update DomainMap.tsx

--- src/components/DomainMap.tsx
+++ src/components/DomainMap.tsx
@@ -2,7 +2,7 @@ const DomainMap = () => {
   return (
     <img
       style={{ width: '100%' }}
-      src="https://cdn.mapcreator.io/published/f2e46cf3186f518ec51d3766/map.png"
+      src="/path/to/your/local/image.png" // Remplacez par une image locale si nécessaire
       alt="Sans Titre 2025-5-8"
       className="h-48 rounded-md object-cover"
     />
