918e7a7a07c70ee987632ed7ec5d5fc13568407a
tiph10 <Tiphanie.moray@gmail.com>
2025-05-08 20:52:58 +0200

    Update DomainMap.tsx

--- src/components/DomainMap.tsx
+++ src/components/DomainMap.tsx
@@ -2,8 +2,8 @@ const DomainMap = () => {
   return (
     <img
       style={{ width: '100%' }}
-      src="/photos/domaine.png" 
-      alt="Photo du domaine"
+      src="https://cdn.mapcreator.io/published/f2e46cf3186f518ec51d3766/map.png" 
+      alt="Carte du Domaine du Clochet"
       className="h-48 rounded-md object-cover"
     />
   );
